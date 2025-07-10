9f50aec071aa4d3a9e45840f570b1bc07a0eced5
tebit0 <evillagran.arriagada@gmail.com>
2025-07-10 01:25:22 -0400

    Update main_plugin.py

--- main_plugin.py
+++ main_plugin.py
@@ -1,3 +1,4 @@
+from qgis.PyQt.QtWidgets import QLineEdit, QPushButton
 from qgis.PyQt.QtWidgets import QAction, QDialog, QVBoxLayout, QLabel
 
 class BufferPlugin:
@@ -12,9 +13,21 @@ class BufferPlugin:
     def run(self):
         dlg = QDialog()
         layout = QVBoxLayout()
-        layout.addWidget(QLabel("¡Funciona! Mi primer plugin"))
+    
+        self.input_distancia = QLineEdit("100")
+        layout.addWidget(QLabel("Distancia del buffer:"))
+        layout.addWidget(self.input_distancia)
+    
+        btn_ejecutar = QPushButton("Crear Buffer")
+        btn_ejecutar.clicked.connect(self.crear_buffer)
+        layout.addWidget(btn_ejecutar)
+    
         dlg.setLayout(layout)
         dlg.exec_()
+        
+    def crear_buffer(self):
+        distancia = float(self.input_distancia.text())
+        print(f"Creando buffer de {distancia} metros")
         
     def unload(self):
         self.iface.removeToolBarIcon(self.action)
