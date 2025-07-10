123f4fd07646599e84f677cdb3173996f3297173
tebit0 <evillagran.arriagada@gmail.com>
2025-07-10 01:15:32 -0400

    Update main_plugin.py

--- main_plugin.py
+++ main_plugin.py
@@ -1,1 +1,21 @@
+from qgis.PyQt.QtWidgets import QAction, QDialog, QVBoxLayout, QLabel
 
+class BufferPlugin:
+    def __init__(self, iface):
+        self.iface = iface
+        
+    def initGui(self):
+        self.action = QAction("Mi Buffer Tool", self.iface.mainWindow())
+        self.action.triggered.connect(self.run)
+        self.iface.addToolBarIcon(self.action)
+        
+    def run(self):
+        dlg = QDialog()
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel("¡Funciona! Mi primer plugin"))
+        dlg.setLayout(layout)
+        dlg.exec_()
+        
+    def unload(self):
+        self.iface.removeToolBarIcon(self.action)
+        del self.action
